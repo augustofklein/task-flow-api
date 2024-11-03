6bfd345ae4106bedd21d116fcd7291632f50252d
Augusto Fernando Klein <augusto_klein@hotmail.com>
2024-11-03 09:49:51 -0300

    Add the initial projects in the solucion

---
+++ TaskFlowManager.WebApi/Program.cs
@@ -0,0 +1,44 @@
+using Serilog;
+using TaskFlowManager.WebApi;
+
+namespace TaskFlowManager.WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateBootstrapLogger();
+
+            Log.Information("Starting application");
+
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+
+                Log.Information("Application finished successfully");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Error to open the application");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, services, configuration) => configuration
+                    .ReadFrom.Configuration(context.Configuration)
+                    .ReadFrom.Services(services)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console())
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ TaskFlowManager.WebApi/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace TaskFlowManager.WebApi
+{
+    public class Startup(IConfiguration configuration)
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddDatabaseInjection(configuration);
+            services.AddControllers();
+
+            //services.ConfigureServicesExtensions(configuration);
+
+            /*
+            services.AddApiVersioning(config =>
+            {
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                config.AssumeDefaultVersionWhenUnspecified = true;
+                config.ReportApiVersions = true;
+            });
+            */
+
+            services.AddHealthChecks();
+            services.AddEndpointsApiExplorer();
+
+            //services.AddMediatorInjection();
+            //services.AddServices();
+            //services.AddQueries();
+            //services.AddRepositories();
+            //services.AddHandles();
+            //services.AddMemoryCache();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //app.UseMiddleware<Middleware>();
+            app.UseRouting();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health");
+            });
+        }
+    }
+}
